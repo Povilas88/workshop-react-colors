b1e2c8a7267336554265484fb79ef9ff8283b739
Povilas <167984240+Povilas88@users.noreply.github.com>
2024-09-13 11:48:37 +0300

    publish

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import { Button } from './components/ColorButton';
+
+function App() {
+  const [colors, setColors] = useState([]);
+  const [lastClicked, setLastClicked] = useState('');
+  const buttonCount = 4;
+
+  const generateHexColor = (index) => {
+    if (lastClicked === index) {
+      return;
+    }
+
+    const hexColor = `#${Math.random().toString(16).slice(2, 8)}`;
+    const backgroundHexColor = `#${Math.random().toString(16).slice(2, 8)}`;
+    const colorArr = [...colors];
+
+    colorArr[index] = hexColor;
+    setColors(colorArr);
+    setLastClicked(index);
+    document.body.style.backgroundColor = backgroundHexColor;
+  };
+
+  const textArr = ['First', 'Second', 'Third', 'Fourth'];
+  const classArr = ['btn btn-primary', 'btn btn-secondary', 'btn btn-success', 'btn btn-warning']
+
+  return (
+    <div className='mainContainer'>
+      <h1>Random color generator</h1>
+      <div className='btnContainer'>
+        {[...Array(buttonCount)].map((_, index) => (
+          <Button
+            key={index}
+            text={textArr[index]}
+            btnClass={classArr[index]}
+            changeColor={() => generateHexColor(index)}
+            color={colors[index]}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ColorButton.jsx
@@ -0,0 +1,7 @@
+export function Button({ text, btnClass, changeColor, color }) {
+    return (
+        <>
+            <button onClick={changeColor} className={btnClass} style={{ background: color }}>{text}</button>
+        </>
+    )
+}
